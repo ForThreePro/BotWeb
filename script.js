a21f7f6c94445013ece14fda1be10840196ab333
YALLICO OWNER <yallico2024@gmail.com>
2026-08-08 22:51:36 -0500

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 const audio = document.getElementById('musica');
 const btn = document.getElementById('btnMusica');
-if(localStorage.getItem('musicaEstado') === 'playing'){
+
+if(audio && localStorage.getItem('musicaEstado') === 'playing'){
   audio.play(); 
   btn.textContent = '🔇';
 }
-btn.onclick = () => {
-  if(audio.paused){
-    audio.play(); 
-    btn.textContent='🔇'; 
-    localStorage.setItem('musicaEstado','playing');
-  } else {
-    audio.pause(); 
-    btn.textContent='🔊'; 
-    localStorage.setItem('musicaEstado','paused');
+
+if(btn){
+  btn.onclick = () => {
+    if(audio.paused){
+      audio.play(); 
+      btn.textContent='🔇'; 
+      localStorage.setItem('musicaEstado','playing');
+    } else {
+      audio.pause(); 
+      btn.textContent='🔊'; 
+      localStorage.setItem('musicaEstado','paused');
+    }
   }
 }
